e4fd78ab49a761b818fa333ec7cfd45b618fcc25
Josip Igrec <me@allypost.net>
2019-06-02 16:31:40 +0200

    Initial commit

---
+++ ispis.php
@@ -0,0 +1,14 @@
+<?php
+
+$niz = array();
+$myfile = fopen("pitanja.txt", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+	$pitanje = fgets($myfile);
+	$odgovor = fgets($myfile);
+	$niz[$pitanje]=$odgovor;
+}
+fclose($myfile);
+foreach($niz as $key => $value) {
+    echo($key."->".$value);
+}
+?>
